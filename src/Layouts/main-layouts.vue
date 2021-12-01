50382575bddd787abb1db762d92372eda0c24345
vlad-Kriajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2021-12-02 00:45:31 +0300

    new

---
+++ src/Layouts/main-layouts.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <NavBar/>
+        <main >
+            <router-view
+            @postFilter="getFilter"
+            :filterChek='filterChek'
+            />
+        </main>
+    </div>
+</template>
+
+
+<script>
+import NavBar from '../components/app/Header/NavBar'
+
+
+export default {
+    name: 'main-layouts',
+    data(){
+        return{
+            filterChek:'',
+        }
+    },
+    components:{
+        NavBar
+    },
+    
+    methods:{
+        getFilter(data){
+            this.filterChek = data
+            
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__header">
+      <div class="home__columnInfo">
+        <div class="column__info">
+          <p class="info__text textNormal">студия промышленного дизайна</p>
+          <div class="info__title textTitle">
+            Наши продукты <br>
+            созданы работать
+          </div>
+        </div>
+        <button class="column__btn textBtn ">
+          <p>Обсудить ваш проект</p> 
+          <div class="line"></div>
+        </button>
+      </div>
+      <div class="home__columnProduct">
+        <div class="column__imgProducts"></div>
+      </div> 
+    </div>
+    <div class="home__slider">
+      <div class="slider__title textNormal">
+        концепция <span class="textBold">Q</span>
+      </div>
+      <VueSlickCarousel :arrows="false" :dots="true">
+          <div 
+          v-for="quote in quotes"
+          :key="quote.id"
+          class="slider__quotes textNormal ml3"
+          >
+            {{quote.title}}
+            <br>
+            {{quote.text}}
+          </div>
+      </VueSlickCarousel>
+    </div>  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    VueSlickCarousel
+  },
+  data(){
+    return{
+      settings:{
+                "dots": true,
+                "dotsClass": "slick-dots custom-dot-class",
+                "edgeFriction": 0.35,
+                "infinite": false,
+                "speed": 500,
+                "slidesToShow": 1,
+                "slidesToScroll": 1
+               },
+      quotes: [
+        {
+          id:1,
+          title: 'Продукт превыше всего.',
+          text:'Либо делаем крутой продукт, либо не делаем его вообще.'
+        },
+        {
+          id:2,
+          title:'Мы ценим честность',
+          text:'Честны перед собой, нашим клиентом и продуктом.'
+        },
+        {
+          id:3,
+          title:'Работаем ради денег',
+          text:'Главная задача проекта - принести вам прибыль. '
+        },
+        {
+          id:4,
+          title:'Клиент не всегда прав',
+          text:'Мы точно знаем, что в некоторых вещах разбираемся лучше.'
+        },
+      ]  
+  }
   }
 }
 </script>
